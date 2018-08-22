2007819a51fc07442de69512d372e84aa9cbade4
Andrew Mainhart <andrewmainhart426@gmail.com>
2018-08-21 20:06:34 -0400

    Initial commit of Spring project.

---
+++ src/main/java/com/drexel/cs/checkers451/Checkers451Application.java
@@ -0,0 +1,12 @@
+package com.drexel.cs.checkers451;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Checkers451Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Checkers451Application.class, args);
+    }
+}
---
+++ src/test/java/com/drexel/cs/checkers451/Checkers451ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.drexel.cs.checkers451;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Checkers451ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

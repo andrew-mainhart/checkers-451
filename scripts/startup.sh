b0485acb0167c17e156581d13484202ddaaf2d1b
Andrew Mainhart <andrewmainhart426@gmail.com>
2018-08-27 19:58:13 -0400

    Testing startup script.

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-java -jar /opt/checkers451/checkers451-0.0.1.jar & echo $! > /opt/checkers451/pid.file &
+java -jar /opt/checkers451/checkers451-0.0.1.jar &
+echo $! > /opt/checkers451/pid.file
